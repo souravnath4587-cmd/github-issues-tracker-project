7b9bb9d8928010df74cd090c8c99b18605ed7be0
souravnath4587-cmd <souravnath4587@gmail.com>
2026-03-07 21:04:21 +0600

    responsive navbar

--- login/login.js
+++ login/login.js
@@ -2,9 +2,9 @@ console.log('login js working...');
 
 const handleLogin=()=>{
     console.log("log in button is click");
-    const userName = document.getElementById('userName').value ;
+    const userName = document.getElementById('userName').value.trim().toLowerCase() ;
     console.log(userName);
-    const password = document.getElementById('password').value ;
+    const password = document.getElementById('password').value.toLowerCase() ;
     console.log(password);
     if(userName === 'admin' && password === 'admin123'){
         alert("Login Successfully...")
